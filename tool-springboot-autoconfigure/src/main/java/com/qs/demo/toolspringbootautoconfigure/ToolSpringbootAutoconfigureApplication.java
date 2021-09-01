68cca997c97474686ded56b09e5b40adc7ec40c0
qiansheng <qiansheng@yunji.ai>
2021-09-01 19:48:32 +0800

    first submit

---
+++ tool-springboot-autoconfigure/src/main/java/com/qs/demo/toolspringbootautoconfigure/ToolSpringbootAutoconfigureApplication.java
@@ -0,0 +1,13 @@
+package com.qs.demo.toolspringbootautoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolSpringbootAutoconfigureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolSpringbootAutoconfigureApplication.class, args);
+    }
+
+}
---
+++ tool-springboot-autoconfigure/src/test/java/com/qs/demo/toolspringbootautoconfigure/ToolSpringbootAutoconfigureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qs.demo.toolspringbootautoconfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolSpringbootAutoconfigureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
