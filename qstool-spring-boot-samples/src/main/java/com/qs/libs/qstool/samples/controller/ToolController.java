d3ea19d2f78e5bb1705ab563eb64ee87abf8bd30
qiansheng <qiansheng@yunji.ai>
2021-09-02 17:30:54 +0800

    first submit

---
+++ qstool-spring-boot-samples/src/main/java/com/qs/libs/qstool/samples/controller/ToolController.java
@@ -0,0 +1,16 @@
+package com.qs.libs.qstool.samples.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tool")
+    public class ToolController {
+
+    @GetMapping("/v1/mvc")
+    public Boolean testInterceptor(){
+        return true;
+    }
+}
